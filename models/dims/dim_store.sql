48e6a78fef598a47c592e50e2c0512f3b03f07d3
yasmin.aly.2022@anderson.ucla.edu <yasmin.aly.2022@anderson.ucla.edu>
2022-03-01 00:08:16 +0000

    init

--- models/dims/dim_store.sql
+++ models/dims/dim_store.sql
@@ -1,16 +1,18 @@
 {{ config(materialized='table') }}
 
 select 
-    stg_store.store_id,
-    stg_address.district,
-    stg_address.address,
-    stg_address.phone,
-
-    stg_staff.last_name
-
+    {{ dbt_utils.surrogate_key(['s.store_id'])}} as STORE_LOCATION_KEY,
+    s.store_id,
+	a.postal_code,
+	c.city,
+	a.district,
+	co.country,
+	a.phone
 from 
-    {{ ref('stg_store')}}
-left join {{ ref('stg_address')}}    
-    on stg_store.address_id = stg_address.address_id
-left join {{ ref('stg_staff')}} 
-    on stg_store.manager_staff_id = stg_staff.staff_id
+    {{ ref('stg_store')}} s
+inner join {{ ref('stg_address')}} a    
+on s.address_id = a.address_id
+inner join {{ ref('stg_city')}} c    
+on a.city_id = c.city_id   
+left join {{ ref('stg_country')}} co
+on c.country_id = co.country_id
