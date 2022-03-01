4448c8aa00c24017f8c26cae291c5bcb2a787039
yasmin.aly.2022@anderson.ucla.edu <yasmin.aly.2022@anderson.ucla.edu>
2022-03-01 20:48:09 +0000

    init

--- models/dims/dim_timeofday.sql
+++ models/dims/dim_timeofday.sql
@@ -4,9 +4,9 @@
 select 
     TIMEADD(minute, SEQ4(), '2017-01-01 00:00:00') as TEMP_TIMESTAMP,
     seq4() as TEMP_SEQUENCE,
-    row_number() over (order by TEMP_SEQUENCE) as TIME_PK,
-    to_time(to_char(TEMP_TIMESTAMP,'HH24:MI:00'),'HH24:MI:00') Hour_24,
-    to_char(TEMP_TIMESTAMP,'HH12:MI:00 PM') HOUR_12,
+    row_number() over (order by TEMP_SEQUENCE) as TIMEOFDAY_KEY,
+    to_time(to_char(TEMP_TIMESTAMP,'HH24:MI:00'),'HH24:MI:00') TIMEOFDAY_24HR,
+    to_char(TEMP_TIMESTAMP,'HH12:MI:00 PM') TIMEOFDAY_12HR,
     hour(TEMP_TIMESTAMP) as HROFDAY,
     minute(TEMP_TIMESTAMP) as MINOFDAY
     from table(generator(rowcount => 1440))
