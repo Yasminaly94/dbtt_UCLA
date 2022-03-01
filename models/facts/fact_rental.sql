b27af8c91e2344073a6761e3493df7117f7ba205
yasmin.aly.2022@anderson.ucla.edu <yasmin.aly.2022@anderson.ucla.edu>
2022-03-01 20:50:42 +0000

    init

--- models/facts/fact_rental.sql
+++ models/facts/fact_rental.sql
@@ -2,7 +2,7 @@
 
 select 
     {{ dbt_utils.surrogate_key(["d.DATE_KEY"])}} as RENTAL_DATE_KEY,
-    {{ dbt_utils.surrogate_key(["od.TIMEOFDAY_KEY"])}} as RENTAL_TIMEOFDAY_KEY,
+    {{ dbt_utils.surrogate_key(["tod.TIMEOFDAY_KEY"])}} as RENTAL_TIMEOFDAY_KEY,
     //HOUR(r.rental_date),
     //r.rental_date,
     r.rental_id,
